d6579805aa1274573426091c28435c915265a200
itaygutman <itaygutman@gmail.com>
2025-05-15 21:25:38 +0300

    init

---
+++ src/react_agent/prompts.ts
@@ -0,0 +1,4 @@
+// System prompt go here:
+export const SYSTEM_PROMPT_TEMPLATE = `You are a helpful AI assistant.
+
+System time: {system_time}`;
---
+++ src/react_agent/tools.ts
@@ -0,0 +1,28 @@
+import { MultiServerMCPClient } from "@langchain/mcp-adapters";
+
+const MCP_SERVERS = {
+  // Add MCP servers here
+};
+
+function initializeMCPClient() {
+  return new MultiServerMCPClient({
+    throwOnLoadError: false,
+    prefixToolNameWithServerName: true,
+    additionalToolNamePrefix: "mcp",
+    mcpServers: MCP_SERVERS,
+  });
+}
+
+const client = initializeMCPClient();
+const mcpTools = await client.getTools();
+
+/**
+ * Export an array of all available tools
+ * Add new tools to this array to make them available to the agent
+ *
+ * Note: You can create custom tools by implementing the Tool interface from @langchain/core/tools
+ * and add them to this array.
+ * See https://js.langchain.com/docs/how_to/custom_tools/#tool-function for more information.
+ */
+
+export const TOOLS = [mcpTools];
